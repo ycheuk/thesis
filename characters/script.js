a9b2b8ade38112aa801cdd1a1392361cdef49261
ycheuk03@gmail.com <ycheuk03@gmail.com>
2024-11-19 18:53:02 -0800

    changed css

--- characters/script.js
+++ characters/script.js
@@ -5,76 +5,76 @@ document.addEventListener("DOMContentLoaded", function () {
 function showContent(section) {
     const contentContainer = document.getElementById('contentContainer');
     const overviewContent = document.getElementById('overview-content');
-    const weaponsContent = document.getElementById('weapons-content');
-    const artifactsContent = document.getElementById('artifacts-content');
-    const constellationsContent = document.getElementById('constellations-content');
+    const interviewsContent = document.getElementById('interviews-content');
+    const gingerbreadContent = document.getElementById('gingerbread-content');
+    const subjectcContent = document.getElementById('subjectc-content');
     const talentsContent = document.getElementById('talents-content');
     const teamsContent = document.getElementById('teams-content');
     const starterContent = document.getElementById('starter-content');
 
     if (section === 'overview') {
         overviewContent.style.display = 'flex';
-        weaponsContent.style.display = 'none';
-        artifactsContent.style.display = 'none';
-        constellationsContent.style.display = 'none';
+        interviewsContent.style.display = 'none';
+        gingerbreadContent.style.display = 'none';
+        subjectcContent.style.display = 'none';
         talentsContent.style.display = 'none';
         teamsContent.style.display = 'none';
         starterContent.style.display = 'none';
-    } else if (section === 'weapons') {
+    } else if (section === 'interviews') {
         overviewContent.style.display = 'none';
-        weaponsContent.style.display = 'block';
-        artifactsContent.style.display = 'none';
-        constellationsContent.style.display = 'none';
+        interviewsContent.style.display = 'block';
+        gingerbreadContent.style.display = 'none';
+        subjectcContent.style.display = 'none';
         talentsContent.style.display = 'none';
         teamsContent.style.display = 'none';
         starterContent.style.display = 'none';
-    } else if (section === 'artifacts') {
+    } else if (section === 'gingerbread') {
         overviewContent.style.display = 'none';
-        weaponsContent.style.display = 'none';
-        artifactsContent.style.display = 'flex';
-        constellationsContent.style.display = 'none';
+        interviewsContent.style.display = 'none';
+        gingerbreadContent.style.display = 'flex';
+        subjectcContent.style.display = 'none';
         talentsContent.style.display = 'none';
         teamsContent.style.display = 'none';
         starterContent.style.display = 'none';
-    } else if (section === 'constellations') {
+    } else if (section === 'subjectc') {
         overviewContent.style.display = 'none';
-        weaponsContent.style.display = 'none';
-        artifactsContent.style.display = 'none';
-        constellationsContent.style.display = 'flex';
+        interviewsContent.style.display = 'none';
+        gingerbreadContent.style.display = 'none';
+        subjectcContent.style.display = 'flex';
         talentsContent.style.display = 'none';
         teamsContent.style.display = 'none';
         starterContent.style.display = 'none';
     } else if (section === 'teams') {
         overviewContent.style.display = 'none';
-        weaponsContent.style.display = 'none';
-        artifactsContent.style.display = 'none';
-        constellationsContent.style.display = 'none';
+        interviewsContent.style.display = 'none';
+        gingerbreadContent.style.display = 'none';
+        subjectcContent.style.display = 'none';
         talentsContent.style.display = 'none';
         teamsContent.style.display = 'flex';
         starterContent.style.display = 'none';
     } else if (section === 'talents') {
         overviewContent.style.display = 'none';
-        weaponsContent.style.display = 'none';
-        artifactsContent.style.display = 'none';
-        constellationsContent.style.display = 'none';
+        interviewsContent.style.display = 'none';
+        gingerbreadContent.style.display = 'none';
+        subjectcContent.style.display = 'none';
         talentsContent.style.display = 'none';
         talentsContent.style.display = 'block';
         teamsContent.style.display = 'none';
         starterContent.style.display = 'none';
     } else if (section === 'starter') {
         overviewContent.style.display = 'none';
-        weaponsContent.style.display = 'none';
-        artifactsContent.style.display = 'none';
-        constellationsContent.style.display = 'none';
+        interviewsContent.style.display = 'none';
+        gingerbreadContent.style.display = 'none';
+        subjectcContent.style.display = 'none';
         talentsContent.style.display = 'none';
         talentsContent.style.display = 'none';
         teamsContent.style.display = 'none';
         starterContent.style.display = 'flex';
     } else {
         overviewContent.style.display = 'none';
-        weaponsContent.style.display = 'none';
-        artifactsContent.style.display = 'none';
-        constellationsContent.style.display = 'none';
+        interviewsContent.style.display = 'none';
+        gingerbreadContent.style.display = 'none';
+        subjectcContent.style.display = 'none';
         teamsContent.style.display = 'none';
         starterContent.style.display = 'none';
 
