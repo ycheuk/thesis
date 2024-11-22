bfb716fd93021e480a28d03775f24643710a0996
ycheuk03@gmail.com <ycheuk03@gmail.com>
2024-11-22 11:03:59 -0800

    added css3

--- characters/script.js
+++ characters/script.js
@@ -8,80 +8,80 @@ function showContent(section) {
     const interviewsContent = document.getElementById('interviews-content');
     const gingerbreadContent = document.getElementById('gingerbread-content');
     const subjectcContent = document.getElementById('subjectc-content');
-    const talentsContent = document.getElementById('talents-content');
+    const genshinContent = document.getElementById('genshin-content');
     const teamsContent = document.getElementById('teams-content');
-    const starterContent = document.getElementById('starter-content');
+    const conclusionContent = document.getElementById('conclusion-content');
 
     if (section === 'overview') {
         overviewContent.style.display = 'flex';
         interviewsContent.style.display = 'none';
         gingerbreadContent.style.display = 'none';
         subjectcContent.style.display = 'none';
-        talentsContent.style.display = 'none';
+        genshinContent.style.display = 'none';
         teamsContent.style.display = 'none';
-        starterContent.style.display = 'none';
+        conclusionContent.style.display = 'none';
     } else if (section === 'interviews') {
         overviewContent.style.display = 'none';
         interviewsContent.style.display = 'block';
         gingerbreadContent.style.display = 'none';
         subjectcContent.style.display = 'none';
-        talentsContent.style.display = 'none';
+        genshinContent.style.display = 'none';
         teamsContent.style.display = 'none';
-        starterContent.style.display = 'none';
+        conclusionContent.style.display = 'none';
     } else if (section === 'gingerbread') {
         overviewContent.style.display = 'none';
         interviewsContent.style.display = 'none';
         gingerbreadContent.style.display = 'flex';
         subjectcContent.style.display = 'none';
-        talentsContent.style.display = 'none';
+        genshinContent.style.display = 'none';
         teamsContent.style.display = 'none';
-        starterContent.style.display = 'none';
+        conclusionContent.style.display = 'none';
     } else if (section === 'subjectc') {
         overviewContent.style.display = 'none';
         interviewsContent.style.display = 'none';
         gingerbreadContent.style.display = 'none';
         subjectcContent.style.display = 'flex';
-        talentsContent.style.display = 'none';
+        genshinContent.style.display = 'none';
         teamsContent.style.display = 'none';
-        starterContent.style.display = 'none';
+        conclusionContent.style.display = 'none';
     } else if (section === 'teams') {
         overviewContent.style.display = 'none';
         interviewsContent.style.display = 'none';
         gingerbreadContent.style.display = 'none';
         subjectcContent.style.display = 'none';
-        talentsContent.style.display = 'none';
+        genshinContent.style.display = 'none';
         teamsContent.style.display = 'flex';
-        starterContent.style.display = 'none';
-    } else if (section === 'talents') {
+        conclusionContent.style.display = 'none';
+    } else if (section === 'genshin') {
         overviewContent.style.display = 'none';
         interviewsContent.style.display = 'none';
         gingerbreadContent.style.display = 'none';
         subjectcContent.style.display = 'none';
-        talentsContent.style.display = 'none';
-        talentsContent.style.display = 'block';
+        genshinContent.style.display = 'none';
+        genshinContent.style.display = 'block';
         teamsContent.style.display = 'none';
-        starterContent.style.display = 'none';
-    } else if (section === 'starter') {
+        conclusionContent.style.display = 'none';
+    } else if (section === 'conclusion') {
         overviewContent.style.display = 'none';
         interviewsContent.style.display = 'none';
         gingerbreadContent.style.display = 'none';
         subjectcContent.style.display = 'none';
-        talentsContent.style.display = 'none';
-        talentsContent.style.display = 'none';
+        genshinContent.style.display = 'none';
+        genshinContent.style.display = 'none';
         teamsContent.style.display = 'none';
-        starterContent.style.display = 'flex';
+        conclusionContent.style.display = 'flex';
     } else {
         overviewContent.style.display = 'none';
         interviewsContent.style.display = 'none';
         gingerbreadContent.style.display = 'none';
         subjectcContent.style.display = 'none';
         teamsContent.style.display = 'none';
-        starterContent.style.display = 'none';
+        conclusionContent.style.display = 'none';
 
         let sectionContent = '';
         switch(section) {
-            case 'talents':
-                sectionContent = 'Your Talents Content Here...';
+            case 'genshin':
+                sectionContent = 'Your genshin Content Here...';
                 break;
             default:
                 sectionContent = 'Default Content';
